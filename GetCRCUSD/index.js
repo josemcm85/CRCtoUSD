c5416660cbe60ade10cba0ed28a53c968d4a6199
Jose Chaves <josemcm85@gmail.com>
2022-05-14 19:07:43 -0600

    Create Json file

--- GetCRCUSD/index.js
+++ GetCRCUSD/index.js
@@ -2,27 +2,26 @@ const request = require("request-promise")
 const cheerio = require("cheerio");
 
 let today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
 today = yyyy + '/' + mm + '/' + dd;
 
 request("https://gee.bccr.fi.cr/indicadoreseconomicos/Cuadros/frmVerCatCuadro.aspx?CodCuadro=400&Idioma=1&FecInicial="+ today + "&FecFinal=" + today + "&Filtro=0", (error, response, html) => {
     if(!error && response.statusCode == 200){
         const $= cheerio.load(html);
 
-
         const datarow= $(".celda400");
-        let output= datarow.text();
-        let fulldata = output.replace(/\s+/g, " ");
-        const myArray = fulldata.split(" ");
-        let fecha = myArray[1] + "/" + myArray[2] + "/" + myArray[3];
-        let compra = myArray[4];
-        let venta = myArray[5];
+        let output= datarow.text().replace(/\s+/g, " ");
+        const myArray = output.split(" ");
 
-        console.log("Fecha: " + fecha);
-        console.log("Compra: " + compra);
-        console.log("Venta: " + venta);
+        let jsonObj = {"fecha" : myArray[1] + "/" + myArray[2] + "/" + myArray[3], 
+                    "compra" : myArray[4], 
+                    "venta" : myArray[5]};
+        
+        let jsonString = JSON.stringify(jsonObj);
+        let fs = require('fs');
+        fs.writeFileSync("ExchangeRate.json",jsonString);
         
     }
 
